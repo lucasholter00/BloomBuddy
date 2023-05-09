6a8a01513c814191e002079e6363eaf9c5530596
Lucas Holter <holter@chalmers.se>
2023-05-09 14:02:54 +0200

    Add functionallity in DataBaseConnection
    setUp: Split setup into two parts to make sure client is not out of
    scope as it was previously in first setup, also add an attribute to
    store client.
    addUser: Check if user with given username exists, if not create a
    document with given username and password, also make 2 empty lists with
    profiles and historicaldata.
    verifyLogin: Check if document with given username and password exists,
    if so, return true, else return false.

--- BloomBuddy/src/main/java/com/group18/BloomBuddy/DataBaseConnection.java
+++ BloomBuddy/src/main/java/com/group18/BloomBuddy/DataBaseConnection.java
@@ -1,3 +1,5 @@
+package com.group18.BloomBuddy;
+
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoException;
@@ -6,30 +8,77 @@ import com.mongodb.ServerApiVersion;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.*;
+
+import java.util.Arrays;
+
 import org.bson.Document;
 
 public class DataBaseConnection {
     String connectionString = "mongodb+srv://BloomBuddy:BloomBuddy@cluster0.23osmgf.mongodb.net/?retryWrites=true&w=majority";    
+    MongoClient client;
     MongoDatabase database;
 
     public DataBaseConnection(){
-        database = setUp();
+        client = setUpClient();
+        database = setUpDatabase(client);
     }
 
-    private MongoDatabase setUp(){
+    private MongoClient setUpClient(){
         ServerApi serverApi = ServerApi.builder().version(ServerApiVersion.V1).build();
         MongoClientSettings settings = MongoClientSettings.builder().applyConnectionString(new ConnectionString(connectionString)).serverApi(serverApi).build();
         // Create a new client and connect to the server
-        try (MongoClient mongoClient = MongoClients.create(settings)) {
-            try {
-                // Send a ping to confirm a successful connection
-                MongoDatabase database = mongoClient.getDatabase("BloomBuddy");
-                database.runCommand(new Document("ping", 1));
-                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
-            } 
-            catch (MongoException e) {
-                e.printStackTrace();
-            }
+        MongoClient mongoClient = MongoClients.create(settings);
+        return mongoClient;
+    }
+
+    private MongoDatabase setUpDatabase(MongoClient client){
+        try {
+            // Send a ping to confirm a successful connection
+            MongoDatabase database = client.getDatabase("BloomBuddy");
+            database.runCommand(new Document("ping", 1));
+            System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
+            return database;
+        } 
+        catch (MongoException e) {
+            e.printStackTrace();
         }
+        return null;
+    } 
+    
+
+    public void query(){
+        // Query the database
+        MongoCollection<Document> collection = database.getCollection("sys_user");
+        Document doc = collection.find().first();
+        System.out.println(doc.toJson());
+    }   
+
+    public boolean addUser(String username, String password){
+        MongoCollection<Document> collection = database.getCollection("sys_user");
+
+        Document query = new Document("username", username);
+        Document result = collection.find(query).first();
+        if (result != null){
+            return false;
+        }
+
+        Document document = new Document("username", username)
+                .append("password", password)
+                .append("profiles", Arrays.asList())
+                .append("historicalData", Arrays.asList());
+        collection.insertOne(document);
+        return true;
+    }
+
+    public boolean verifyLogin(String username, String password){
+        MongoCollection<Document> collection = database.getCollection("sys_user");
+        Document query = new Document("username", username).append("password", password);
+        Document result = collection.find(query).first();
+        if(result != null){
+            return true;
+        }
+        return false;
     }
 }
+
