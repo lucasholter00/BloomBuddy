c2c59e81c3a1ba52b1ce09b8ce18d8118cdd524d
corolo <corolo@chalmers.se>
2023-05-10 16:31:51 +0200

    Modify attributes in Profile.java - last time watered Date object and water frequency as an integer. Add method returning if watering is needed.
    - Attributes enable the functionality of saving the last time watered and will in future commits be saved in the database
    - Profile.waterNeeded returns true if plant needs to be watered and false if not, which will act as a trigger to notifications

--- BloomBuddy/src/main/java/com/group18/BloomBuddy/Profile.java
+++ BloomBuddy/src/main/java/com/group18/BloomBuddy/Profile.java
@@ -1,10 +1,32 @@
 package com.group18.BloomBuddy;
 
+import java.util.Date;
+
 public class Profile {
     private SensorSettings sensorSettings;
+    private Date lastWatered; //Date of when the plant was last watered
+    private int waterFrequency; //How often the water should be watered, in terms of days
+
 
     public Profile(SensorSettings sensorSettings) {
         this.sensorSettings = sensorSettings;
+        this.lastWatered = null; //Initialize as null, could be better ways to initialize this
+        this.waterFrequency = 0; //Initialize as 0, i.e. no interval to water have been chosen yet by the user
+    }
+
+    public boolean waterNeeded(){
+        if (lastWatered==null){
+            //If lastWatered have not been initialized we will assume that the plant needs watering
+            return true;
+        }
+
+        Date todaysDate = new Date();
+
+        long elapsedTime = todaysDate.getTime()-getLastWatered().getTime(); //the time elapsed in milliseconds since last watered date
+
+        long waterFreqToMilli = (long) waterFrequency * 24 * 60 * 60 * 1000; //24 * 60 * 60 * 1000 = numbers of miliseconds in a day (24h)
+
+        return elapsedTime > waterFreqToMilli;
 
     }
 
@@ -16,4 +38,16 @@ public class Profile {
         this.sensorSettings = sensorSettings;
     }
 
+    public Date getLastWatered() {
+        return lastWatered;
+    }
+    public void setLastWatered(Date lastWatered) {
+        this.lastWatered = lastWatered;
+    }
+    public int getWaterFrequency() {
+        return waterFrequency;
+    }
+    public void setWaterFrequency(int waterFrequency) {
+        this.waterFrequency = waterFrequency;
+    }
 }
