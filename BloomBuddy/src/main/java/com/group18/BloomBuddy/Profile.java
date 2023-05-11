2d1d2e3a95acb7120aaa9763405a2bf59b244495
corolo <corolo@chalmers.se>
2023-05-11 11:55:24 +0200

    LocalDateTime in use instead of Date, with following changes to calculation methods

--- BloomBuddy/src/main/java/com/group18/BloomBuddy/Profile.java
+++ BloomBuddy/src/main/java/com/group18/BloomBuddy/Profile.java
@@ -1,13 +1,13 @@
 package com.group18.BloomBuddy;
 
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 public class Profile {
     private SensorSettings sensorSettings;
-    private Date lastWatered; //Date of when the plant was last watered
+    //Date of when the plant was last watered
+    private LocalDateTime lastWatered;
     private int waterFrequency; //How often the water should be watered, in terms of days
-
-
     public Profile(SensorSettings sensorSettings) {
         this.sensorSettings = sensorSettings;
         this.lastWatered = null; //Initialize as null, could be better ways to initialize this
@@ -20,13 +20,17 @@ public class Profile {
             return true;
         }
 
-        Date todaysDate = new Date();
+        LocalDateTime todayDate = LocalDateTime.now();
 
-        long elapsedTime = todaysDate.getTime()-getLastWatered().getTime(); //the time elapsed in milliseconds since last watered date
+        long elapsedTime = ChronoUnit.MILLIS.between(lastWatered, todayDate); //the time elapsed in milliseconds since last watered date
 
         long waterFreqToMilli = (long) waterFrequency * 24 * 60 * 60 * 1000; //24 * 60 * 60 * 1000 = numbers of miliseconds in a day (24h)
 
         return elapsedTime > waterFreqToMilli;
+
+    }
+
+    public void saveLastWatered(){
 
     }
 
@@ -38,10 +42,10 @@ public class Profile {
         this.sensorSettings = sensorSettings;
     }
 
-    public Date getLastWatered() {
+    public LocalDateTime getLastWatered() {
         return lastWatered;
     }
-    public void setLastWatered(Date lastWatered) {
+    public void setLastWatered(LocalDateTime lastWatered) {
         this.lastWatered = lastWatered;
     }
     public int getWaterFrequency() {
