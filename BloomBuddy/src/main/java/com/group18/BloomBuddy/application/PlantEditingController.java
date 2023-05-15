bf6a342a259ec0194a5096bc7c246c3d077ddaec
Vilmer Hedin <vilmerh@student.chalmers.se>
2023-05-15 14:18:32 +0200

    Added handle method to allow for editing of existing profiles. Separated the validation of the bounds to a new method to prevent changes to the thresholds before saving.

--- BloomBuddy/src/main/java/com/group18/BloomBuddy/application/PlantEditingController.java
+++ BloomBuddy/src/main/java/com/group18/BloomBuddy/application/PlantEditingController.java
@@ -48,58 +48,90 @@ public class PlantEditingController extends SceneSwitcher {
         stage.show();
     }
     @FXML
-    private Profile saveSettings(ActionEvent event) {
-        SensorSettings test = new SensorSettings(10, 20, 10, 20, 10,20, 10,20);
-        Profile newProfile = new Profile(test);
-        try {
-            float temperatureLowerBound = Float.parseFloat(TempLowBound.getText());
-            float temperatureUpperBound = Float.parseFloat(TempUpBound.getText());
-            float humidityLowerBound = Float.parseFloat(HumLowBound.getText());
-            float humidityUpperBound = Float.parseFloat(HumUpBound.getText());
-            float moistureLowerBound = Float.parseFloat(MoistLowBound.getText());
-            float moistureUpperBound = Float.parseFloat(MoistUpBound.getText());
-            float lightLowerBound = 0;
-            float lightUpperBound = 0;
-            if(LightLow.isSelected()){
-                lightLowerBound = 0;
-                lightUpperBound = 512;
-            } else if (LightHigh.isSelected()) {
-                lightLowerBound = 512;
-                lightUpperBound = 2000;
-            }
+    private void handleSaveSettingsButton(ActionEvent event) {
+        saveSettings(event, new Profile(new SensorSettings(10, 20, 10, 20, 10,20,10,20)));
+    }
+    @FXML
+    private void saveSettings(ActionEvent event, Profile profile) {
 
-            //Validate the bounds
-            if (temperatureLowerBound >= temperatureUpperBound) {
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("Invalid Input");
-                alert.setHeaderText(null);
-                alert.setContentText("Temperature lower bound must be less than upper bound.");
-                alert.showAndWait();
-                return null;
-            }
-            if (humidityLowerBound >= humidityUpperBound) {
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("Invalid Input");
-                alert.setHeaderText(null);
-                alert.setContentText("Humidity lower bound must be less than upper bound.");
-                alert.showAndWait();
-                return null;            }
-            if (moistureLowerBound >= moistureUpperBound) {
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("Invalid Input");
-                alert.setHeaderText(null);
-                alert.setContentText("moisture lower bound must be less than upper bound.");
-                alert.showAndWait();
-                return null;
-            }
-            //if all validations pass, create the new SensorSettings object.
-            SensorSettings settings = new SensorSettings(temperatureLowerBound, temperatureUpperBound, moistureLowerBound, moistureUpperBound, lightLowerBound, lightUpperBound, humidityLowerBound, humidityUpperBound);
-            newProfile.setSensorSettings(settings);
+            try {
+                if(validateBounds()) {
+                    SensorSettings settings = profile.getSensorSettings();
+                    settings.setTemperatureLowerBound(Float.parseFloat(TempLowBound.getText()));
+                    settings.setTemperatureUpperBound(Float.parseFloat(TempUpBound.getText()));
+                    settings.setHumidityLowerBound(Float.parseFloat(HumLowBound.getText()));
+                    settings.setHumidityUpperBound(Float.parseFloat(HumUpBound.getText()));
+                    settings.setMoistureLowerBound(Float.parseFloat(MoistLowBound.getText()));
+                    settings.setMoistureUpperBound(Float.parseFloat(MoistUpBound.getText()));
+                    if (LightLow.isSelected()) {
+                        settings.setLightLowerBound(0);
+                        settings.setLightUpperBound(512);
+                    } else if (LightHigh.isSelected()) {
+                        settings.setLightLowerBound(0);
+                        settings.setLightUpperBound(2000);
+                    }
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Success Dialog");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Settings were successfully saved.");
+                    alert.showAndWait();
 
-        } catch (NumberFormatException e) {
-            // Handle the case where a value is not a valid float or any other expected data type.
+                    System.out.println("New settings saved to profile:");
+                    System.out.println("Temperature Lower Bound: " + settings.getTemperatureLowerBound());
+                    System.out.println("Temperature Upper Bound: " + settings.getTemperatureUpperBound());
+                    System.out.println("Humidity Lower Bound: " + settings.getHumidityLowerBound());
+                    System.out.println("Humidity Upper Bound: " + settings.getHumidityUpperBound());
+                    System.out.println("Moisture Lower Bound: " + settings.getMoistureLowerBound());
+                    System.out.println("Moisture Upper Bound: " + settings.getMoistureUpperBound());
+                    System.out.println("Light Lower Bound: " + settings.getLightLowerBound());
+                    System.out.println("Light Upper Bound: " + settings.getLightUpperBound());
+                }
+
+
+            } catch (NumberFormatException e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Invalid Input");
+                alert.setHeaderText(null);
+                alert.setContentText("Please enter valid numbers.");
+                alert.showAndWait();
+            }
         }
-        return newProfile;
+
+    public Boolean validateBounds(){
+        boolean valid = true;
+        float temperatureLowerBound = Float.parseFloat(TempLowBound.getText());
+        float temperatureUpperBound = Float.parseFloat(TempUpBound.getText());
+        float humidityLowerBound = Float.parseFloat(HumLowBound.getText());
+        float humidityUpperBound = Float.parseFloat(HumUpBound.getText());
+        float moistureLowerBound = Float.parseFloat(MoistLowBound.getText());
+        float moistureUpperBound = Float.parseFloat(MoistUpBound.getText());
+        if (temperatureLowerBound >= temperatureUpperBound) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Invalid Input");
+            alert.setHeaderText(null);
+            alert.setContentText("Temperature lower bound must be less than upper bound.");
+            alert.showAndWait();
+            valid = true;
+
+        }
+        if (humidityLowerBound >= humidityUpperBound) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Invalid Input");
+            alert.setHeaderText(null);
+            alert.setContentText("Humidity lower bound must be less than upper bound.");
+            alert.showAndWait();
+            valid = false;
+        }
+        if (moistureLowerBound >= moistureUpperBound) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Invalid Input");
+            alert.setHeaderText(null);
+            alert.setContentText("moisture lower bound must be less than upper bound.");
+            alert.showAndWait();
+            valid = false;
+
+        }
+        return valid;
     }
 
 }
