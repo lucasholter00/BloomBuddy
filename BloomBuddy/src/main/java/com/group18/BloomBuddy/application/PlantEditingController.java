81994154ff478c63088c9611c46833683afdb2f7
Vilmer Hedin <vilmerh@student.chalmers.se>
2023-05-15 12:03:13 +0200

    Allow the user to create their own customizable plant/crop thresholds through the JavaFX application.

--- BloomBuddy/src/main/java/com/group18/BloomBuddy/application/PlantEditingController.java
+++ BloomBuddy/src/main/java/com/group18/BloomBuddy/application/PlantEditingController.java
@@ -1,14 +1,37 @@
 package com.group18.BloomBuddy.application;
 
+import com.group18.BloomBuddy.Profile;
+import com.group18.BloomBuddy.SensorSettings;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
 
 public class PlantEditingController extends SceneSwitcher {
+    public RadioButton LightLow;
+    public RadioButton LightHigh;
+    @FXML
+    private TextField TempLowBound;
+    @FXML
+    private TextField TempUpBound;
+    @FXML
+    private TextField HumLowBound;
+    @FXML
+    private TextField HumUpBound;
+    @FXML
+    private TextField MoistLowBound;
+    @FXML
+    private TextField MoistUpBound;
+    @FXML
+    private Button saveSettingsButton;
+
     public void show(Stage stage) throws IOException {
         URL fxmlResource = getClass().getResource("/plantEditingScene.fxml");
         FXMLLoader loader = new FXMLLoader();
@@ -24,4 +47,34 @@ public class PlantEditingController extends SceneSwitcher {
         stage.setFullScreen(false);
         stage.show();
     }
+    @FXML
+    private Profile saveSettings(ActionEvent event) {
+        SensorSettings test = new SensorSettings(10, 20, 10, 20, 10,20, 10,20);
+        Profile newProfile = new Profile(test);
+        try {
+            float temperatureLowerBound = Float.parseFloat(TempLowBound.getText());
+            float temperatureUpperBound = Float.parseFloat(TempUpBound.getText());
+            float humidityLowerBound = Float.parseFloat(HumLowBound.getText());
+            float humidityUpperBound = Float.parseFloat(HumUpBound.getText());
+            float moistureLowerBound = Float.parseFloat(MoistLowBound.getText());
+            float moistureUpperBound = Float.parseFloat(MoistUpBound.getText());
+            float lightLowerBound = 0;
+            float lightUpperBound = 0;
+            if(LightLow.isSelected()){
+                lightLowerBound = 0;
+                lightUpperBound = 512;
+            } else if (LightHigh.isSelected()) {
+                lightLowerBound = 512;
+                lightUpperBound = 2000;
+            }
+            //if all validations pass, create the new SensorSettings object.
+            SensorSettings settings = new SensorSettings(temperatureLowerBound, temperatureUpperBound, moistureLowerBound, moistureUpperBound, lightLowerBound, lightUpperBound, humidityLowerBound, humidityUpperBound);
+            newProfile.setSensorSettings(settings);
+
+        } catch (NumberFormatException e) {
+            // Handle the case where a value is not a valid float or any other expected data type.
+        }
+        return newProfile;
+    }
+
 }
