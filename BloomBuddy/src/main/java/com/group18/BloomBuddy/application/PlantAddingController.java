2ba5b6b6d63428e0c4c294be50660983bf45b24c
Rasmus Nygren <rasmusny@net.chalmers.se>
2023-05-18 12:30:44 +0200

    Added the functionality to the plantCreationScene and changed the validation in the plantEditingScene to stay consistent.

--- BloomBuddy/src/main/java/com/group18/BloomBuddy/application/PlantAddingController.java
+++ BloomBuddy/src/main/java/com/group18/BloomBuddy/application/PlantAddingController.java
@@ -1,14 +1,32 @@
 package com.group18.BloomBuddy.application;
 
+import com.group18.BloomBuddy.Profile;
+import com.group18.BloomBuddy.SensorSettings;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.eclipse.paho.client.mqttv3.MqttException;
 
 import java.io.IOException;
 import java.net.URL;
 
-public class PlantAddingController extends SceneSwitcher  {
+public class PlantAddingController extends SceneSwitcher {
+    public TextField plantName;
+    public RadioButton LightLow;
+    public RadioButton LightHigh;
+    public TextField TempLowBound;
+    public TextField TempUpBound;
+    public TextField HumLowBound;
+    public TextField HumUpBound;
+    public TextField MoistLowBound;
+    public TextField MoistUpBound;
+    public Label accountCreationLabel;
+
     public void show(Stage stage) throws IOException {
         URL fxmlResource = getClass().getResource("/plantAddingScene.fxml");
         FXMLLoader loader = new FXMLLoader();
@@ -24,4 +42,69 @@ public class PlantAddingController extends SceneSwitcher  {
         stage.setFullScreen(false);
         stage.show();
     }
+
+    public void createPlantProfile(ActionEvent actionEvent) throws MqttException, IOException {
+        try {
+            if (validateProfile()) {
+                float lightLower = 0;
+                float lightHigher = 0;
+                if (LightLow.isSelected()) {
+                    lightLower = 0;
+                    lightHigher = 512;
+                } else if (LightHigh.isSelected()) {
+                    lightLower = 512;
+                    lightHigher = 2000;
+                }
+                SensorSettings sensorSettings = new SensorSettings(
+                        Float.parseFloat(TempLowBound.getText()),
+                        Float.parseFloat(TempUpBound.getText()),
+                        Float.parseFloat(MoistLowBound.getText()),
+                        Float.parseFloat(MoistUpBound.getText()),
+                        lightLower,
+                        lightHigher,
+                        Float.parseFloat(HumLowBound.getText()),
+                        Float.parseFloat(HumUpBound.getText())
+                );
+                accountCreationLabel.setText("Profile was successfully created.");
+                Profile profile = new Profile(sensorSettings, plantName.getText());
+            }
+        } catch (NumberFormatException e){
+            accountCreationLabel.setText("Please enter valid numbers.");
+        }
+    }
+    public boolean validateProfile() {
+        boolean valid = true;
+        if (plantName.getText().isBlank()) {
+            accountCreationLabel.setText("Enter a name for your plant.");
+            valid = false;
+        }
+        if (TempLowBound.getText().isBlank() || TempUpBound.getText().isBlank()
+                || HumLowBound.getText().isBlank() || HumUpBound.getText().isBlank()
+                || MoistLowBound.getText().isBlank() || MoistUpBound.getText().isBlank()) {
+            accountCreationLabel.setText("One or more bounds are blank. Please enter values for all bounds.");
+            valid = false;
+        } else {
+            float temperatureLowerBound = Float.parseFloat(TempLowBound.getText());
+            float temperatureUpperBound = Float.parseFloat(TempUpBound.getText());
+            float humidityLowerBound = Float.parseFloat(HumLowBound.getText());
+            float humidityUpperBound = Float.parseFloat(HumUpBound.getText());
+            float moistureLowerBound = Float.parseFloat(MoistLowBound.getText());
+            float moistureUpperBound = Float.parseFloat(MoistUpBound.getText());
+
+            if (temperatureLowerBound >= temperatureUpperBound) {
+                accountCreationLabel.setText("Temperature lower bound must be less than upper bound.");
+                valid = false;
+            }
+            if (humidityLowerBound >= humidityUpperBound) {
+                accountCreationLabel.setText("Humidity lower bound must be less than upper bound.");
+                valid = false;
+            }
+            if (moistureLowerBound >= moistureUpperBound) {
+                accountCreationLabel.setText("Moisture lower bound must be less than upper bound.");
+                valid = false;
+            }
+        }
+        return valid;
+    }
+
 }
